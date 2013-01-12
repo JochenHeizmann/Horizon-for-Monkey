0c23de08b641556992939e6d58facbd872bef51e
Ingo <ingo@baab.de>
2013-01-12 22:37:50 +0100

    added GetHostname() for Target 'glfw'.

--- native/util.glfw.cpp
+++ native/util.glfw.cpp
@@ -1,17 +1,34 @@
 #include <ctime>
 
+#define HOST_NAME_MAX 1024
+
 class util {
 public:
-	void static NavigateToUrl(String url) {
-		system("open http://www.intermediaware.com");
-	}
+    void static NavigateToUrl(String url) {
+        system("open http://www.intermediaware.com");
+    }
 
-	int static GetTimestamp() {
-		return std::time(0);
-	}
+    int static GetTimestamp() {
+        return std::time(0);
+    }
 
     bool static IsTabletDevice()
     {
         return true;
+    }
+
+    String static GetHostname() {
+        return "localhost";
+
+        // char name[HOST_NAME_MAX + 1];
+        // return gethostname(name, sizeof name) == -1 || printf("%s\n", name) < 0 ? EXIT_FAILURE : EXIT_SUCCESS;
+
+        /*
+        if( gethostname(name, sizeof name) == -1 ) {
+            return printf("%s", name);
+        } else {
+            return "localhost";
+        }
+        */
     }
 };
